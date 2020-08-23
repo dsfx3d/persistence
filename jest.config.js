81716b411208cb48a595e7a34d002ecb64d397c9
dsfx3d <dsfx3d@gmail.com>
2020-08-24 01:08:34 +0530

    :rotating_light: configure coverage for alpha release

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  'coverageThreshold': {
-    'global': {
-      'branches': 100,
-      'functions': 100,
-      'lines': 100,
-      'statements': 100
-    }
-  }
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
 }
