8d877f29b7d1e1f2106d0bcc9e4c29785f04618a
dsfx3d <dsfx3d@gmail.com>
2020-08-23 23:02:36 +0530

    :fire: remove adapter interface

--- src/types/adapters.ts
+++ src/types/adapters.ts
@@ -1,11 +1,3 @@
-export interface Adapter {
-  key(n: number): string
-  getItem<T = any>(key: string, fallback: T): T
-  setItem<T = any>(key: string, value: T): void
-  removeItem(key: string): void
-  clear(): void
-}
-
 export interface AsyncAdapter {
   key(n: number): Promise<string>
   getItem<T = any>(key: string, fallback: T): Promise<T>
