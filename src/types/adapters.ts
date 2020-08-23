260f219836b10b66a72d81d4c64cbfed35a75a10
dsfx3d <dsfx3d@gmail.com>
2020-08-23 22:51:41 +0530

    :zap: add async adapter types

--- src/types/adapters.ts
+++ src/types/adapters.ts
@@ -6,6 +6,14 @@ export interface Adapter {
   clear(): void
 }
 
+export interface AsyncAdapter {
+  key(n: number): Promise<string>
+  getItem<T = any>(key: string, fallback: T): Promise<T>
+  setItem<T = any>(key: string, value: T): Promise<void>
+  removeItem(key: string): Promise<void>
+  clear(): Promise<void>
+}
+
 export interface AdapterFactory<T = any> {
   build(): T
 }
